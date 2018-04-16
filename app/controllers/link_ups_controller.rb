54d7e7bcaee555468e62bb1379b835bd446d73e7
github username <github email address>
2018-04-16 17:01:27 -0400

    finishedform

--- app/controllers/link_ups_controller.rb
+++ app/controllers/link_ups_controller.rb
@@ -1,31 +1,37 @@
 class LinkUpsController < ApplicationController
   def index
+    @locations = Location.all
   end
-
-  def show
-  end
+  #
+  # def show
+  # end
 
   def new
     @link_up = LinkUp.new
+    @locations = Location.all
+    @interests = Interest.all
+    @users = User.all
   end
 
   def create
     @link_up = LinkUp.create(link_up_params)
-    @locations = Location.all
+    if @link_up.valid?
+      redirect_to @link_up
+    end
   end
 
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
+  # def edit
+  # end
+  #
+  # def update
+  # end
+  #
+  # def destroy
+  # end
 
   private
 
   def link_up_params
-    params.require(:name, :location_id, :interes_id)
+    params.require(:link_up).permit(:id, :name, :location_id, :interest_id, :user_id, :guest_id)
   end
 end
