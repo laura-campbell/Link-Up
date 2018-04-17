fe56b1505df4482b7e80cbcb1d3e61efabd5713d
Arthur Lurye <arthurlurye@gmail.com>
2018-04-17 15:00:25 -0400

    worked on showpage and buttons

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20180416175042) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
+    t.integer "linkup_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
